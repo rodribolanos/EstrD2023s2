084dc9fa4f99c23914b9a81229b634160f621cb9
Rodrigo Bolaños <rodrimanuelbw@gmail.com>
2023-08-14 19:24:50 -0300

    Hasta ejercicio Dia de la semana

--- Practica1.hs
+++ Practica1.hs
@@ -26,14 +26,75 @@ maxDelPar (a, b) = if a > b
     4- sumar (maxDelPar (divisionYResto 100 20)) (sucesor 4)
 -}
 
--- Ejercicio 3
+-- Ejercicio 3.1
 
 data Dir = Norte | Sur | Este | Oeste
     deriving Show
 
 --A 
 opuesto :: Dir -> Dir 
-opuesto Norte = Este 
-opuesto Este = Sur 
-opuesto Sur = Oeste 
-opuesto Oeste = Norte 
+opuesto Norte = Sur 
+opuesto Este = Oeste
+opuesto Sur = Norte 
+opuesto Oeste = Este 
+
+--B 
+iguales :: Dir -> Dir -> Bool
+iguales Norte Norte  = True
+iguales Este Este    = True
+iguales Sur Sur      = True 
+iguales Oeste Oeste  = True 
+iguales _ _ = False
+
+--C 
+siguiente :: Dir -> Dir 
+siguiente a = 
+    case a of 
+    Norte -> Este 
+    Este -> Sur
+    Sur -> Oeste 
+    Oeste -> Norte
+
+-- Ejercicio 3.2
+
+data DiaDeSemana = Lunes | Martes | Miercoles | Jueves | Viernes | Sabado | Domingo 
+    deriving Show
+
+primerDiaDeSemana = Lunes
+ultimoDiaDeSemana = Domingo
+
+--A 
+primeroYUltimoDia :: (DiaDeSemana, DiaDeSemana)
+primeroYUltimoDia = (primerDiaDeSemana, ultimoDiaDeSemana)
+
+--B 
+empiezaConM :: DiaDeSemana -> Bool 
+empiezaConM Martes = True 
+empiezaConM Miercoles = True 
+empiezaConM _ = False
+
+--C 
+vieneDespues :: DiaDeSemana -> DiaDeSemana -> Bool
+vieneDespues a b =  iguales ((siguienteDia a ) b)
+
+
+igualesDia :: DiaDeSemana -> DiaDeSemana -> Bool
+igualesDia Lunes Lunes          = True
+igualesDia Martes Martes        = True
+igualesDia Miercoles Miercoles  = True 
+igualesDia Jueves Jueves        = True 
+igualesDia Viernes Viernes      = True 
+igualesDia Sabado Sabado        = True
+iguales _ _ = False
+
+
+siguienteDia :: DiaDeSemana -> DiaDeSemana 
+siguienteDia a = 
+    case a of 
+    Lunes -> Martes 
+    Martes -> Miercoles
+    Miercoles -> Jueves
+    Jueves -> Viernes
+    Viernes -> Sabado 
+    Sabado -> Domingo 
+    Domingo -> Lunes
